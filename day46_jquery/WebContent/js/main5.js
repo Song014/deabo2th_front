cd6998612bef6020a645ae4af865067edfad5c25
Song014 <ghgsb6200@gmail.com>
2023-06-01 11:11:05 +0900

    Feat: FrontLecture all add

---
+++ day46_jquery/WebContent/js/main5.js
@@ -0,0 +1,18 @@
+function sum2(x,y){
+    return x+y;
+}
+function sum(x,y){
+    console.log(x+y);
+}
+const a=sum2(10,20);
+const b = sum2(5,10);
+console.log(a+b);
+
+
+sum(1,3);
+sum(5,7);
+
+
+
+
+
